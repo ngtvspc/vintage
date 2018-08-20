fc8cf2c656dbff90981c607f7e60002bfb2f1a80
Shawn Shojaie <shawn@simplelegal.com>
2018-08-20 14:12:12 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""
+Setup module for Pylint plugin for Django.
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='pylint-django',
+    url='https://github.com/PyCQA/pylint-django',
+    author='Shawn Shojaie',
+    author_email='',
+    description='',
+    long_description='',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pylint<2.0',
+    ],
+    license='BSD',
+    classifiers=[],
+    keywords=['pylint', 'django', 'plugin'],
+    zip_safe=False,
+)
